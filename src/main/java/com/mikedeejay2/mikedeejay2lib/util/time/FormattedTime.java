ccafe0b7379a6ed3969be268780c4bf93b1327c2
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2020-10-08 16:59:00 -0400

    Add FormattedTime

---
+++ src/main/java/com/mikedeejay2/mikedeejay2lib/util/time/FormattedTime.java
@@ -0,0 +1,27 @@
+package com.mikedeejay2.mikedeejay2lib.util.time;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * A simple formatting class for formatting the current time
+ *
+ * @author Mikedeejay2
+ */
+public final class FormattedTime
+{
+    // The formatter that will format the current time
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    // A reference to the current LocalDateTime
+    private static final LocalDateTime now = LocalDateTime.now();
+
+    /**
+     * Get the current time formatted as "yyyy/MM/dd HH:mm:ss"
+     *
+     * @return The current formatted time
+     */
+    public static String getTime()
+    {
+        return formatter.format(now);
+    }
+}
