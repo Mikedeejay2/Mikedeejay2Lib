ea72a0e0e29547d81dfe17e6c394d5e3e8314890
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2020-12-22 12:51:10 -0500

    Add Predicate to block iterator

--- src/main/java/com/mikedeejay2/mikedeejay2lib/util/block/BlockIterator.java
+++ src/main/java/com/mikedeejay2/mikedeejay2lib/util/block/BlockIterator.java
@@ -4,8 +4,11 @@ import com.mikedeejay2.mikedeejay2lib.PluginBase;
 import com.mikedeejay2.mikedeejay2lib.runnable.EnhancedRunnable;
 import com.mikedeejay2.mikedeejay2lib.util.math.MathUtil;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class BlockIterator
 {
@@ -18,8 +21,9 @@ public class BlockIterator
      * @param zWidth   The width of the iteration in the Z direction
      * @param runnable The <tt>BlockRunnable</tt> that will be ran at each block
      * @param plugin   The plugin's instance to use
+     * @param typePredicate The <tt>Predicate</tt> condition
      */
-    public static void iterateBlocks(Location loc, int xWidth, int yWidth, int zWidth, BlockRunnable runnable, PluginBase plugin)
+    public static void iterateBlocks(Location loc, int xWidth, int yWidth, int zWidth, BlockRunnable runnable, PluginBase plugin, Predicate<Material> typePredicate)
     {
         EnhancedRunnable iteration = new EnhancedRunnable()
         {
@@ -27,7 +31,57 @@ public class BlockIterator
             public void onRun()
             {
                 List<Location> locs = MathUtil.getCubeFilledLocations(loc, xWidth, yWidth, zWidth, 1);
-                locs.forEach(location -> runnable.run(location, location.getBlock()));
+                for(Location location : locs)
+                {
+                    Block block = location.getBlock();
+                    Material material = block.getType();
+                    if(typePredicate != null && !typePredicate.test(material)) continue;
+                    runnable.run(location, location.getBlock());
+                }
+            }
+        };
+        iteration.runTask(plugin);
+    }
+
+    /**
+     * Iterate through all blocks around a location in a square.
+     *
+     * @param loc      The location in which to iterate blocks around
+     * @param xWidth   The width of the iteration in the X direction
+     * @param yWidth   The width of the iteration in the Y direction
+     * @param zWidth   The width of the iteration in the Z direction
+     * @param runnable The <tt>BlockRunnable</tt> that will be ran at each block
+     * @param plugin   The plugin's instance to use
+     */
+    public static void iterateBlocks(Location loc, int xWidth, int yWidth, int zWidth, BlockRunnable runnable, PluginBase plugin)
+    {
+        iterateBlocks(loc, xWidth, yWidth, zWidth, runnable, plugin, null);
+    }
+
+    /**
+     * Iterate through all blocks around a location in a hollow square.
+     *
+     * @param loc1     The first location of the cube
+     * @param loc2     The second location of the cube
+     * @param runnable The <tt>BlockRunnable</tt> that will be ran at each block
+     * @param plugin   The plugin's instance to use
+     * @param typePredicate The <tt>Predicate</tt> condition
+     */
+    public static void iterateBlocksHollow(Location loc1, Location loc2, BlockRunnable runnable, PluginBase plugin, Predicate<Material> typePredicate)
+    {
+        EnhancedRunnable iteration = new EnhancedRunnable()
+        {
+            @Override
+            public void onRun()
+            {
+                List<Location> locs = MathUtil.getCubeHollowLocations(loc1, loc2, 1);
+                for(Location location : locs)
+                {
+                    Block block = location.getBlock();
+                    Material material = block.getType();
+                    if(typePredicate != null && !typePredicate.test(material)) continue;
+                    runnable.run(location, location.getBlock());
+                }
             }
         };
         iteration.runTask(plugin);
@@ -43,13 +97,33 @@ public class BlockIterator
      */
     public static void iterateBlocksHollow(Location loc1, Location loc2, BlockRunnable runnable, PluginBase plugin)
     {
+        iterateBlocksHollow(loc1, loc2, runnable, plugin, null);
+    }
+
+    /**
+     * Iterate through all blocks around a location in a square.
+     *
+     * @param loc1     The first location of the cube
+     * @param loc2     The second location of the cube
+     * @param runnable The <tt>BlockRunnable</tt> that will be ran at each block
+     * @param plugin   The plugin's instance to use
+     * @param typePredicate The <tt>Predicate</tt> condition
+     */
+    public static void iterateBlocks(Location loc1, Location loc2, BlockRunnable runnable, PluginBase plugin, Predicate<Material> typePredicate)
+    {
         EnhancedRunnable iteration = new EnhancedRunnable()
         {
             @Override
             public void onRun()
             {
-                List<Location> locs = MathUtil.getCubeHollowLocations(loc1, loc2, 1);
-                locs.forEach(location -> runnable.run(location, location.getBlock()));
+                List<Location> locs = MathUtil.getCubeFilledLocations(loc1, loc2, 1);
+                for(Location location : locs)
+                {
+                    Block block = location.getBlock();
+                    Material material = block.getType();
+                    if(typePredicate != null && !typePredicate.test(material)) continue;
+                    runnable.run(location, location.getBlock());
+                }
             }
         };
         iteration.runTask(plugin);
@@ -65,13 +139,35 @@ public class BlockIterator
      */
     public static void iterateBlocks(Location loc1, Location loc2, BlockRunnable runnable, PluginBase plugin)
     {
+        iterateBlocks(loc1, loc2, runnable, plugin, null);
+    }
+
+    /**
+     * Iterate through all blocks around a location in a hollow square.
+     *
+     * @param loc      The location in which to iterate blocks around
+     * @param xWidth   The width of the iteration in the X direction
+     * @param yWidth   The width of the iteration in the Y direction
+     * @param zWidth   The width of the iteration in the Z direction
+     * @param runnable The <tt>BlockRunnable</tt> that will be ran at each block
+     * @param plugin   The plugin's instance to use
+     * @param typePredicate The <tt>Predicate</tt> condition
+     */
+    public static void iterateBlocksHollow(Location loc, int xWidth, int yWidth, int zWidth, BlockRunnable runnable, PluginBase plugin, Predicate<Material> typePredicate)
+    {
         EnhancedRunnable iteration = new EnhancedRunnable()
         {
             @Override
             public void onRun()
             {
-                List<Location> locs = MathUtil.getCubeFilledLocations(loc1, loc2, 1);
-                locs.forEach(location -> runnable.run(location, location.getBlock()));
+                List<Location> locs = MathUtil.getCubeHollowLocations(loc, xWidth, yWidth, zWidth, 1);
+                for(Location location : locs)
+                {
+                    Block block = location.getBlock();
+                    Material material = block.getType();
+                    if(typePredicate != null && !typePredicate.test(material)) continue;
+                    runnable.run(location, location.getBlock());
+                }
             }
         };
         iteration.runTask(plugin);
@@ -89,13 +185,35 @@ public class BlockIterator
      */
     public static void iterateBlocksHollow(Location loc, int xWidth, int yWidth, int zWidth, BlockRunnable runnable, PluginBase plugin)
     {
+        iterateBlocksHollow(loc, xWidth, yWidth, zWidth, runnable, plugin, null);
+    }
+
+    /**
+     * Iterate through all blocks around a location in a square outline.
+     *
+     * @param loc      The location in which to iterate blocks around
+     * @param xWidth   The width of the iteration in the X direction
+     * @param yWidth   The width of the iteration in the Y direction
+     * @param zWidth   The width of the iteration in the Z direction
+     * @param runnable The <tt>BlockRunnable</tt> that will be ran at each block
+     * @param plugin   The plugin's instance to use
+     * @param typePredicate The <tt>Predicate</tt> condition
+     */
+    public static void iterateBlocksOutline(Location loc, int xWidth, int yWidth, int zWidth, BlockRunnable runnable, PluginBase plugin, Predicate<Material> typePredicate)
+    {
         EnhancedRunnable iteration = new EnhancedRunnable()
         {
             @Override
             public void onRun()
             {
-                List<Location> locs = MathUtil.getCubeHollowLocations(loc, xWidth, yWidth, zWidth, 1);
-                locs.forEach(location -> runnable.run(location, location.getBlock()));
+                List<Location> locs = MathUtil.getCubeOutlineLocations(loc, xWidth, yWidth, zWidth, 1);
+                for(Location location : locs)
+                {
+                    Block block = location.getBlock();
+                    Material material = block.getType();
+                    if(typePredicate != null && !typePredicate.test(material)) continue;
+                    runnable.run(location, location.getBlock());
+                }
             }
         };
         iteration.runTask(plugin);
@@ -113,18 +231,38 @@ public class BlockIterator
      */
     public static void iterateBlocksOutline(Location loc, int xWidth, int yWidth, int zWidth, BlockRunnable runnable, PluginBase plugin)
     {
+        iterateBlocksOutline(loc, xWidth, yWidth, zWidth, runnable, plugin, null);
+    }
+
+
+    /**
+     * Iterate through all blocks around a location in a square outline.
+     *
+     * @param loc1     The first location of the cube
+     * @param loc2     The second location of the cube
+     * @param runnable The <tt>BlockRunnable</tt> that will be ran at each block
+     * @param plugin   The plugin's instance to use
+     * @param typePredicate The <tt>Predicate</tt> condition
+     */
+    public static void iterateBlocksOutline(Location loc1, Location loc2, BlockRunnable runnable, PluginBase plugin, Predicate<Material> typePredicate)
+    {
         EnhancedRunnable iteration = new EnhancedRunnable()
         {
             @Override
             public void onRun()
             {
-                List<Location> locs = MathUtil.getCubeOutlineLocations(loc, xWidth, yWidth, zWidth, 1);
-                locs.forEach(location -> runnable.run(location, location.getBlock()));
+                List<Location> locs = MathUtil.getCubeOutlineLocations(loc1, loc2, 1);
+                for(Location location : locs)
+                {
+                    Block block = location.getBlock();
+                    Material material = block.getType();
+                    if(typePredicate != null && !typePredicate.test(material)) continue;
+                    runnable.run(location, location.getBlock());
+                }
             }
         };
         iteration.runTask(plugin);
     }
-
 
     /**
      * Iterate through all blocks around a location in a square outline.
@@ -136,13 +274,33 @@ public class BlockIterator
      */
     public static void iterateBlocksOutline(Location loc1, Location loc2, BlockRunnable runnable, PluginBase plugin)
     {
+        iterateBlocksOutline(loc1, loc2, runnable, plugin, null);
+    }
+
+    /**
+     * Iterate through all blocks in a sphere.
+     *
+     * @param loc      The location in which to iterate blocks around
+     * @param radius   The radius of the sphere to iterate through
+     * @param runnable The <tt>BlockRunnable</tt> that will be ran at each block
+     * @param plugin   The plugin's instance to use
+     * @param typePredicate The <tt>Predicate</tt> condition
+     */
+    public static void iterateBlocksSphere(Location loc, double radius, BlockRunnable runnable, PluginBase plugin, Predicate<Material> typePredicate)
+    {
         EnhancedRunnable iteration = new EnhancedRunnable()
         {
             @Override
             public void onRun()
             {
-                List<Location> locs = MathUtil.getCubeOutlineLocations(loc1, loc2, 1);
-                locs.forEach(location -> runnable.run(location, location.getBlock()));
+                List<Location> locs = MathUtil.getSphereFilledLocations(loc, radius, 1);
+                for(Location location : locs)
+                {
+                    Block block = location.getBlock();
+                    Material material = block.getType();
+                    if(typePredicate != null && !typePredicate.test(material)) continue;
+                    runnable.run(location, location.getBlock());
+                }
             }
         };
         iteration.runTask(plugin);
@@ -158,13 +316,33 @@ public class BlockIterator
      */
     public static void iterateBlocksSphere(Location loc, double radius, BlockRunnable runnable, PluginBase plugin)
     {
+        iterateBlocksSphere(loc, radius, runnable, plugin, null);
+    }
+
+    /**
+     * Iterate through all blocks in a hollow sphere.
+     *
+     * @param loc      The location in which to iterate blocks around
+     * @param radius   The radius of the sphere to iterate through
+     * @param runnable The <tt>BlockRunnable</tt> that will be ran at each block
+     * @param plugin   The plugin's instance to use
+     * @param typePredicate The <tt>Predicate</tt> condition
+     */
+    public static void iterateBlocksSphereHollow(Location loc, double radius, BlockRunnable runnable, PluginBase plugin, Predicate<Material> typePredicate)
+    {
         EnhancedRunnable iteration = new EnhancedRunnable()
         {
             @Override
             public void onRun()
             {
-                List<Location> locs = MathUtil.getSphereFilledLocations(loc, radius, 1);
-                locs.forEach(location -> runnable.run(location, location.getBlock()));
+                List<Location> locs = MathUtil.getSphereHollowLocations(loc, radius, 1);
+                for(Location location : locs)
+                {
+                    Block block = location.getBlock();
+                    Material material = block.getType();
+                    if(typePredicate != null && !typePredicate.test(material)) continue;
+                    runnable.run(location, location.getBlock());
+                }
             }
         };
         iteration.runTask(plugin);
@@ -180,15 +358,45 @@ public class BlockIterator
      */
     public static void iterateBlocksSphereHollow(Location loc, double radius, BlockRunnable runnable, PluginBase plugin)
     {
+        iterateBlocksSphereHollow(loc, radius, runnable, plugin, null);
+    }
+
+    /**
+     * Iterate through all blocks of a list of Locations.
+     *
+     * @param locs The List of Locations to iterate through
+     * @param runnable The <tt>BlockRunnable</tt> that will be ran at each block
+     * @param plugin   The plugin's instance to use
+     * @param typePredicate The <tt>Predicate</tt> condition
+     */
+    public static void iterateBlocks(List<Location> locs, BlockRunnable runnable, PluginBase plugin, Predicate<Material> typePredicate)
+    {
         EnhancedRunnable iteration = new EnhancedRunnable()
         {
             @Override
             public void onRun()
             {
-                List<Location> locs = MathUtil.getSphereHollowLocations(loc, radius, 1);
-                locs.forEach(location -> runnable.run(location, location.getBlock()));
+                for(Location location : locs)
+                {
+                    Block block = location.getBlock();
+                    Material material = block.getType();
+                    if(typePredicate != null && !typePredicate.test(material)) continue;
+                    runnable.run(location, location.getBlock());
+                }
             }
         };
         iteration.runTask(plugin);
+    }
+
+    /**
+     * Iterate through all blocks of a list of Locations.
+     *
+     * @param locs The List of Locations to iterate through
+     * @param runnable The <tt>BlockRunnable</tt> that will be ran at each block
+     * @param plugin   The plugin's instance to use
+     */
+    public static void iterateBlocks(List<Location> locs, BlockRunnable runnable, PluginBase plugin)
+    {
+        iterateBlocks(locs, runnable, plugin, null);
     }
 }
