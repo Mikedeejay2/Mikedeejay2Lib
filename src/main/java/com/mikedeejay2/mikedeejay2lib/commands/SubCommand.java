fe33cbf3358f56cc18e1a1b9ebe858f6e420a62f
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-01-09 11:56:47 -0500

    Fix SubCommand

--- src/main/java/com/mikedeejay2/mikedeejay2lib/commands/SubCommand.java
+++ src/main/java/com/mikedeejay2/mikedeejay2lib/commands/SubCommand.java
@@ -15,14 +15,14 @@ public interface SubCommand
      * @param sender The CommandSender that sent the command
      * @param args   The arguments sent with the command
      */
-    public abstract void onCommand(CommandSender sender, String[] args);
+    void onCommand(CommandSender sender, String[] args);
 
     /**
      * The name of the subcommand
      *
      * @return The name of the subcommand
      */
-    public abstract String name();
+    String name();
 
     /**
      * Any info that the subcommand has
@@ -30,26 +30,26 @@ public interface SubCommand
      * @param sender The CommandSender that requested the info. If null, use default lang.
      * @return Any info that the subcommand has
      */
-    public abstract String info(CommandSender sender);
+    String info(CommandSender sender);
 
     /**
      * Get the list of aliases that this subcommand has
      *
      * @return All aliases of subcommand
      */
-    public abstract String[] aliases();
+    String[] aliases();
 
     /**
      * Get the permission that is required to run this command
      *
      * @return The permission that is required to run this command, null if none
      */
-    public abstract String permission();
+    String permission();
 
     /**
      * Returns whether this subcommand requires a player or not.
      *
      * @return Whether a player is required to run this command
      */
-    public abstract boolean playerRequired();
+    boolean playerRequired();
 }
