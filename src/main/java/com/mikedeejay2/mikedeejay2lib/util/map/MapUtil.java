8ddb692c12191e84039dec5c0de3a75b0c0d9873
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-02-26 16:40:20 -0500

    Add MapUtil

---
+++ src/main/java/com/mikedeejay2/mikedeejay2lib/util/map/MapUtil.java
@@ -0,0 +1,50 @@
+package com.mikedeejay2.mikedeejay2lib.util.map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.meta.MapMeta;
+import org.bukkit.map.MapView;
+
+/**
+ * Utility class for anything to do with maps. (Minecraft maps, not Java maps.)
+ *
+ * @author Mikedeejay2
+ */
+public final class MapUtil
+{
+    /**
+     * Increment the map size of a map.
+     * <p>
+     * Ex. <tt>CLOSE</tt> size goes to <tt>NORMAL</tt> and so forth.
+     *
+     * @param meta The input <tt>MapMeta</tt> to be incremented
+     */
+    public static void incrementMapSize(MapMeta meta)
+    {
+        MapView view = meta.getMapView();
+        MapView newView = Bukkit.createMap(view.getWorld());
+        newView.setCenterX(view.getCenterX());
+        newView.setCenterZ(view.getCenterZ());
+        newView.setLocked(view.isLocked());
+        newView.setTrackingPosition(view.isTrackingPosition());
+        newView.setUnlimitedTracking(view.isUnlimitedTracking());
+        switch(view.getScale())
+        {
+            case CLOSEST:
+                newView.setScale(MapView.Scale.CLOSE);
+                break;
+            case CLOSE:
+                newView.setScale(MapView.Scale.NORMAL);
+                break;
+            case NORMAL:
+                newView.setScale(MapView.Scale.FAR);
+                break;
+            case FAR:
+                newView.setScale(MapView.Scale.FARTHEST);
+                break;
+            case FARTHEST:
+                newView.setScale(MapView.Scale.CLOSEST);
+                break;
+        }
+        meta.setMapView(newView);
+    }
+}
