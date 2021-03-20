8a3f3f2b51c38c3acf9e2ed325a7ef14f65ab971
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-03-20 12:49:24 -0400

    Add Javadocs to IColorFormatter

--- src/main/java/com/mikedeejay2/mikedeejay2lib/util/chat/IColorFormatter.java
+++ src/main/java/com/mikedeejay2/mikedeejay2lib/util/chat/IColorFormatter.java
@@ -1,5 +1,10 @@
 package com.mikedeejay2.mikedeejay2lib.util.chat;
 
+/**
+ * Blank interface for categorizing different color formatters.
+ *
+ * @author Mikedeejay2
+ */
 public interface IColorFormatter
 {
 }
