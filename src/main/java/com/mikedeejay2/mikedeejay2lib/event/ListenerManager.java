d3ecf4a374b684951960289692d8c9d04ae796bd
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-04-17 00:50:14 -0400

    Readd ListenerManager, it's still a useful data structure in some circumstances

---
+++ src/main/java/com/mikedeejay2/mikedeejay2lib/event/ListenerManager.java
@@ -0,0 +1,56 @@
+package com.mikedeejay2.mikedeejay2lib.event;
+
+import com.mikedeejay2.mikedeejay2lib.BukkitPlugin;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+
+import java.util.ArrayList;
+
+/**
+ * A class that manages the registration and referencing of Listeners for a plugin
+ *
+ * @author Mikedeejay2
+ */
+public class ListenerManager
+{
+    protected final BukkitPlugin plugin;
+    private final ArrayList<Listener> listeners;
+
+    public ListenerManager(BukkitPlugin plugin)
+    {
+        this.plugin = plugin;
+        listeners = new ArrayList<>();
+    }
+
+    /**
+     * Add a <tt>Listener</tt> to this manager
+     *
+     * @param listener The listener to add
+     */
+    public void addListener(Listener listener)
+    {
+        listeners.add(listener);
+    }
+
+    /**
+     * Register all added listeners. <p>
+     *
+     * This should only be called once! Add all listeners before running
+     * this method.
+     */
+    public void registerAll()
+    {
+        PluginManager manager = plugin.getServer().getPluginManager();
+        listeners.forEach(listener -> manager.registerEvents(listener, plugin));
+    }
+
+    /**
+     * Get the <tt>ArrayList</tt> of <tt>Listeners</tt>
+     *
+     * @return All <tt>Listeners</tt> that this manager holds
+     */
+    public ArrayList<Listener> getListeners()
+    {
+        return listeners;
+    }
+}
