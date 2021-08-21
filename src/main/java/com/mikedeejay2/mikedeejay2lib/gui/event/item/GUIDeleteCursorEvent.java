7c6bdd78fe2afd856364a03f7c2d791a00edcebb
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-08-20 22:53:07 -0400

    Add GUIDeleteCursorEvent

---
+++ src/main/java/com/mikedeejay2/mikedeejay2lib/gui/event/item/GUIDeleteCursorEvent.java
@@ -0,0 +1,36 @@
+package com.mikedeejay2.mikedeejay2lib.gui.event.item;
+
+import com.mikedeejay2.mikedeejay2lib.gui.GUIContainer;
+import com.mikedeejay2.mikedeejay2lib.gui.event.GUIEvent;
+import com.mikedeejay2.mikedeejay2lib.item.ItemBuilder;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.function.Supplier;
+
+/**
+ * Event that deletes the item in the player's cursor
+ *
+ * @author Mikedeejay2
+ */
+public class GUIDeleteCursorEvent implements GUIEvent
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @param event The event of the click
+     * @param gui   The GUI that the event took place in
+     */
+    @Override
+    public void execute(InventoryClickEvent event, GUIContainer gui)
+    {
+        Player player = (Player) event.getWhoClicked();
+        InventoryView inventory = player.getOpenInventory();
+        inventory.setCursor(null);
+    }
+}
