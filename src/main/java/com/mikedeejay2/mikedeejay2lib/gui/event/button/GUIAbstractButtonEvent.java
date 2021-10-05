4c80cdad2b7f34ea03460dd99e222b046d45a08e
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-10-04 20:34:37 -0400

    Add GUIButtonEvent

---
+++ src/main/java/com/mikedeejay2/mikedeejay2lib/gui/event/button/GUIAbstractButtonEvent.java
@@ -0,0 +1,81 @@
+package com.mikedeejay2.mikedeejay2lib.gui.event.button;
+
+import com.mikedeejay2.mikedeejay2lib.gui.event.GUIEvent;
+import org.bukkit.event.inventory.ClickType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Abstract GUI Button event for button types to extend.
+ *
+ * @author Mikedeejay2
+ */
+public abstract class GUIAbstractButtonEvent implements GUIEvent
+{
+    /**
+     * A List of the accepted {@link ClickType}s that will activate the button.
+     */
+    protected final List<ClickType> acceptedClicks;
+
+    /**
+     * Constructor for {@link GUIAbstractButtonEvent}
+     *
+     * @param acceptedClicks The accepted {@link ClickType}s of the button
+     */
+    public GUIAbstractButtonEvent(ClickType... acceptedClicks)
+    {
+        this.acceptedClicks = new ArrayList<>(Arrays.asList(acceptedClicks));
+    }
+
+    /**
+     * Get List of the accepted {@link ClickType}s that will activate the button.
+     *
+     * @return The list of <code>ClickTypes</code>
+     */
+    public List<ClickType> getAcceptedClicks()
+    {
+        return acceptedClicks;
+    }
+
+    /**
+     * Add a {@link ClickType} to this button
+     *
+     * @param click The <code>ClickType</code> to add
+     */
+    public void addClick(ClickType click)
+    {
+        acceptedClicks.add(click);
+    }
+
+    /**
+     * Add multiple {@link ClickType}s to this button
+     *
+     * @param clicks The <code>ClickTypes</code> to add
+     */
+    public void addClicks(ClickType... clicks)
+    {
+        acceptedClicks.addAll(Arrays.asList(clicks));
+    }
+
+    /**
+     * Remove a {@link ClickType} from this button
+     *
+     * @param click The <code>ClickType</code> to remove
+     */
+    public void removeClick(ClickType click)
+    {
+        acceptedClicks.remove(click);
+    }
+
+    /**
+     * Remove multiple {@link ClickType}s from this button
+     *
+     * @param clicks The <code>ClickTypes</code> to remove
+     */
+    public void removeClicks(ClickType... clicks)
+    {
+        acceptedClicks.removeAll(Arrays.asList(clicks));
+    }
+}
---
+++ src/main/java/com/mikedeejay2/mikedeejay2lib/gui/event/button/GUIButtonEvent.java
@@ -0,0 +1,76 @@
+package com.mikedeejay2.mikedeejay2lib.gui.event.button;
+
+import com.mikedeejay2.mikedeejay2lib.gui.event.GUIEventInfo;
+import org.apache.commons.lang.Validate;
+import org.bukkit.event.inventory.ClickType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
+
+/**
+ * A simple button that executes a {@link Consumer} on click
+ *
+ * @author Mikedeejay2
+ */
+public class GUIButtonEvent extends GUIAbstractButtonEvent
+{
+    /**
+     * The consumer that is run when the button is clicked
+     */
+    protected Consumer<GUIEventInfo> consumer;
+
+    /**
+     * Construct a new <code>GUIButtonEvent</code>
+     *
+     * @param consumer The consumer that is run when the button is clicked
+     * @param acceptedClicks The accepted {@link ClickType}s of the button
+     */
+    public GUIButtonEvent(@NotNull Consumer<GUIEventInfo> consumer, ClickType... acceptedClicks)
+    {
+        super(acceptedClicks);
+        Validate.notNull(consumer, "Button consumer can not be null");
+        this.consumer = consumer;
+    }
+
+    /**
+     * Construct a new <code>GUIButtonEvent</code>
+     *
+     * @param consumer The consumer that is run when the button is clicked
+     */
+    public GUIButtonEvent(@NotNull Consumer<GUIEventInfo> consumer)
+    {
+        this(consumer, ClickType.LEFT, ClickType.RIGHT, ClickType.MIDDLE, ClickType.SHIFT_LEFT, ClickType.SHIFT_RIGHT);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param info {@link GUIEventInfo} of the event
+     */
+    @Override
+    public void execute(GUIEventInfo info)
+    {
+        consumer.accept(info);
+    }
+
+    /**
+     * Get the consumer that is run when the button is clicked
+     *
+     * @return The consumer for the button click
+     */
+    public Consumer<GUIEventInfo> getConsumer()
+    {
+        return consumer;
+    }
+
+    /**
+     * Set the consumer that is run when the button is clicked
+     *
+     * @param consumer The new consumer
+     */
+    public void setConsumer(@NotNull Consumer<GUIEventInfo> consumer)
+    {
+        Validate.notNull(consumer, "Button consumer can not be null");
+        this.consumer = consumer;
+    }
+}
