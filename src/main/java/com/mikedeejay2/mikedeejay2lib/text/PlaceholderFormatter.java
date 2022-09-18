9290c1207cdf260c9fc653304ed6efe02fb3fae1
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2022-09-17 22:28:11 -0400

    PlaceholderFormatter Javadocs

--- src/main/java/com/mikedeejay2/mikedeejay2lib/text/PlaceholderFormatter.java
+++ src/main/java/com/mikedeejay2/mikedeejay2lib/text/PlaceholderFormatter.java
@@ -7,38 +7,90 @@ import org.bukkit.entity.Player;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Formats Strings with configurable and stackable placeholders.
+ * <p>
+ * Usage: <code>%placeholder% -&gt; replacement</code>
+ * <p>
+ * Example: <code>"TPS: %tps%, MSPT: %mspt%" -&gt; "TPS: 19.99, MSPT: 10.53</code>
+ *
+ * @author Mikedeejay2
+ */
 public final class PlaceholderFormatter implements Cloneable {
+    /**
+     * The Map of String placeholder keys to {@link Text} replacement values.
+     */
     private Map<String, Text> placeholders;
 
     private PlaceholderFormatter() {
         this.placeholders = new HashMap<>();
     }
 
-    public static PlaceholderFormatter of(String value, Text replacement) {
-        return new PlaceholderFormatter().and(value, replacement);
+    /**
+     * Get a <code>PlaceholderFormatter</code> of a specified key and replacement
+     *
+     * @param key The placeholder key to find
+     * @param replacement The replacement {@link Text}
+     * @return The new <code>PlaceholderFormatter</code>
+     */
+    public static PlaceholderFormatter of(String key, Text replacement) {
+        return new PlaceholderFormatter().and(key, replacement);
     }
 
-    public static PlaceholderFormatter of(String value, String replacement) {
-        return of(value, Text.of(replacement));
+    /**
+     * Get a <code>PlaceholderFormatter</code> of a specified key and replacement
+     *
+     * @param key The placeholder key to find
+     * @param replacement The replacement String
+     * @return The new <code>PlaceholderFormatter</code>
+     */
+    public static PlaceholderFormatter of(String key, String replacement) {
+        return of(key, Text.of(replacement));
     }
 
-    public PlaceholderFormatter and(String value, Text replacement) {
-        Validate.notNull(value, "Tried to create placeholder of null value");
+    /**
+     * Add a key to this formatter
+     *
+     * @param key The placeholder key to find
+     * @param replacement The replacement {@link Text}
+     * @return This <code>PlaceholderFormatter</code>
+     */
+    public PlaceholderFormatter and(String key, Text replacement) {
+        Validate.notNull(key, "Tried to create placeholder of null key");
         Validate.notNull(replacement, "Tried to create placeholder of null replacement");
-        placeholders.put(value, replacement);
+        placeholders.put(key, replacement);
         return this;
     }
 
+    /**
+     * Add all keys from another <code>PlaceholderFormatter</code> to this formatter
+     *
+     * @param formatter The <code>PlaceholderFormatter</code> to add
+     * @return This <code>PlaceholderFormatter</code>
+     */
     public PlaceholderFormatter and(PlaceholderFormatter formatter) {
         Validate.notNull(formatter, "Tried to create placeholder from null placeholder");
         placeholders.putAll(formatter.placeholders);
         return this;
     }
 
-    public PlaceholderFormatter and(String value, String replacement) {
-        return and(value, Text.of(replacement));
+    /**
+     * Add a key to this formatter
+     *
+     * @param key The placeholder key to find
+     * @param replacement The replacement String
+     * @return This <code>PlaceholderFormatter</code>
+     */
+    public PlaceholderFormatter and(String key, String replacement) {
+        return and(key, Text.of(replacement));
     }
 
+    /**
+     * Format a String with this formatter
+     *
+     * @param string The String to be formatted
+     * @return The formatted String
+     */
     public String format(String string) {
         for(String value : placeholders.keySet()) {
             string = format(string, value, placeholders.get(value).get());
@@ -46,6 +98,13 @@ public final class PlaceholderFormatter implements Cloneable {
         return string;
     }
 
+    /**
+     * Format a String with this formatter
+     *
+     * @param player The player, for use with translatable text
+     * @param string The String to be formatted
+     * @return The formatted String
+     */
     public String format(Player player, String string) {
         Validate.notNull(player, "Tried to format a String for a null player");
         for(String value : placeholders.keySet()) {
@@ -54,6 +113,13 @@ public final class PlaceholderFormatter implements Cloneable {
         return string;
     }
 
+    /**
+     * Format a String with this formatter
+     *
+     * @param sender The <code>CommandSender</code>, for use with translatable text
+     * @param string The String to be formatted
+     * @return The formatted String
+     */
     public String format(CommandSender sender, String string) {
         Validate.notNull(sender, "Tried to format a String for a null CommandSender");
         for(String value : placeholders.keySet()) {
@@ -62,6 +128,13 @@ public final class PlaceholderFormatter implements Cloneable {
         return string;
     }
 
+    /**
+     * Format a String with this formatter
+     *
+     * @param locale The locale, for use with translatable text
+     * @param string The String to be formatted
+     * @return The formatted String
+     */
     public String format(String locale, String string) {
         Validate.notNull(locale, "Tried to format a String for a null locale");
         for(String value : placeholders.keySet()) {
@@ -70,10 +143,23 @@ public final class PlaceholderFormatter implements Cloneable {
         return string;
     }
 
-    private String format(String string, String value, String replacement) {
-        return string.replaceAll("%" + value + "%", replacement);
+    /**
+     * The internal format method. Find all placeholders named <code>key</code> and replace them.
+     *
+     * @param string The String to format
+     * @param key The key of the placeholders to replace
+     * @param replacement The replacement String
+     * @return The formatted String
+     */
+    private String format(String string, String key, String replacement) {
+        return string.replaceAll("%" + key + "%", replacement);
     }
 
+    /**
+     * Clone this <code>PlaceholderFormatter</code>
+     *
+     * @return The cloned <code>PlaceholderFormatter</code>
+     */
     @Override
     public PlaceholderFormatter clone() {
         PlaceholderFormatter formatter;
