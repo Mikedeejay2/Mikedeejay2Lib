a791dcb0fb9f43385a8ca5dbc84c6c6d704c2ff0
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2022-09-30 23:16:32 -0400

    SystemDetails JavaDocs

--- src/main/java/com/mikedeejay2/mikedeejay2lib/util/debug/SystemDetails.java
+++ src/main/java/com/mikedeejay2/mikedeejay2lib/util/debug/SystemDetails.java
@@ -4,14 +4,34 @@ import java.lang.management.ManagementFactory;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class SystemDetails {
+/**
+ * Utility class for getting basic system details
+ *
+ * @author Mikedeejay2
+ */
+public final class SystemDetails {
+    /**
+     * The operating system as a String. Formatted as <code>os.name (os.arch) version os.version</code>
+     */
     public static final String OPERATING_SYSTEM = String.format(
         "%s (%s) version %s", System.getProperty("os.name"), System.getProperty("os.arch"), System.getProperty("os.version"));
+    /**
+     * The Java version as a String. Formatted as <code>java.version, java.vendor</code>
+     */
     public static final String JAVA_VERSION = String.format(
         "%s, %s", System.getProperty("java.version"), System.getProperty("java.vendor"));
+    /**
+     * The Java VM Version as a String. Formatted as <code>java.vm.name (java.vm.info), java.vm.vendor</code>
+     */
     public static final String JAVA_VM_VERSION = String.format(
         "%s (%s), %s", System.getProperty("java.vm.name"), System.getProperty("java.vm.info"), System.getProperty("java.vm.vendor"));
 
+    /**
+     * Get the current memory as a String. Formatted as<code>freeMemory bytes (freeMemory MiB) / totalMemory bytes
+     * (totalMemory MiB) up to maxMemory bytes (maxMemory MiB)</code>
+     *
+     * @return The formatted memory String
+     */
     public static String getMemory() {
         final Runtime runtime = Runtime.getRuntime();
         final long maxMemory = runtime.maxMemory();
@@ -24,10 +44,20 @@ public class SystemDetails {
                              maxMemory, maxMemory / 1048576L);
     }
 
+    /**
+     * Get the count of processor as a String
+     *
+     * @return The processor count as a String
+     */
     public static String getProcessorCount() {
         return String.valueOf(Runtime.getRuntime().availableProcessors());
     }
 
+    /**
+     * Get the JVM flags being used currently. Formatted as <code>size total; [...]</code>
+     *
+     * @return The JVM flags as a String
+     */
     public static String getJVMFlags() {
         final List<String> flags = ManagementFactory
             .getRuntimeMXBean()
